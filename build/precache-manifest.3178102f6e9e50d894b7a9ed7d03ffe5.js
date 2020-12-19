dabbef6844b7c8798819cdc54912737e5a8c2202
wilsons39 <wilsonsaputras39@gmail.com>
2020-12-20 00:58:14 +0700

    build deploy

---
+++ build/precache-manifest.3178102f6e9e50d894b7a9ed7d03ffe5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377ec60fdfb686e836ab9a5161f90fc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f44f257dac5c0626bf89",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "b5a0783c0eb992a29a59",
+    "url": "/static/css/main.310a3801.chunk.css"
+  },
+  {
+    "revision": "f44f257dac5c0626bf89",
+    "url": "/static/js/2.b01fb0fb.chunk.js"
+  },
+  {
+    "revision": "40d6e8d5e466f7b3cfe5f6299c69152f",
+    "url": "/static/js/2.b01fb0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a0783c0eb992a29a59",
+    "url": "/static/js/main.9c98d21e.chunk.js"
+  },
+  {
+    "revision": "7e267852ea3c11350fa9",
+    "url": "/static/js/runtime-main.5c533bcc.js"
+  },
+  {
+    "revision": "9effd5214439cd2bc75c696511837c1a",
+    "url": "/static/media/videoplayback.9effd521.mp3"
+  }
+]);
