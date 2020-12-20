6a42f634bccc108d55b0f82ea2d695cd82cd0f97
wilsons39 <wilsonsaputras39@gmail.com>
2020-12-21 06:00:58 +0700

    new deploy

---
+++ build/precache-manifest.6b4800d77cc7ab792f00ee59f946b5fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbb016847c5c6bfe8dbe157eff2d91b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee44e80a4ed2d54d44bd",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "91496c2a72a62f13ace4",
+    "url": "/static/css/main.f356ffc2.chunk.css"
+  },
+  {
+    "revision": "ee44e80a4ed2d54d44bd",
+    "url": "/static/js/2.1295c687.chunk.js"
+  },
+  {
+    "revision": "40d6e8d5e466f7b3cfe5f6299c69152f",
+    "url": "/static/js/2.1295c687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91496c2a72a62f13ace4",
+    "url": "/static/js/main.02d0d647.chunk.js"
+  },
+  {
+    "revision": "7e267852ea3c11350fa9",
+    "url": "/static/js/runtime-main.5c533bcc.js"
+  },
+  {
+    "revision": "9c9fab5d0f40176b64bb8937cc69ae6f",
+    "url": "/static/media/29.-Happy.9c9fab5d.png"
+  },
+  {
+    "revision": "a77a0e024757dee582b128cb3439f8a1",
+    "url": "/static/media/39.-Technologies.a77a0e02.png"
+  },
+  {
+    "revision": "d5f4a2a56b7077fc43e3f45c8a33c548",
+    "url": "/static/media/46.-Reporting.d5f4a2a5.png"
+  },
+  {
+    "revision": "7cf3d5a19ec90335140b39a98c75d600",
+    "url": "/static/media/59.-Transporting.7cf3d5a1.png"
+  },
+  {
+    "revision": "9effd5214439cd2bc75c696511837c1a",
+    "url": "/static/media/videoplayback.9effd521.mp3"
+  }
+]);
--- src/assets/index.js
+++ src/assets/index.js
@@ -7,5 +7,9 @@ import LinkedinLogo from './icons/linkedin logo.png'
 import BehanceLogo from './icons/behance logo.png'
 import music from './music/videoplayback.mp3'
 import happy29 from './images/29.-Happy.png'
+import technologies39 from './images/39.-Technologies.png'
+import reporting46 from './images/46.-Reporting.png'
+import transporting59 from './images/59.-Transporting.png'
 
-export {SkineaseLogo,FacebookLogo,TwitterLogo,InstagramLogo,YoutubeLogo,LinkedinLogo,BehanceLogo,music,happy29}
+export {SkineaseLogo,FacebookLogo,TwitterLogo,InstagramLogo,
+    YoutubeLogo,LinkedinLogo,BehanceLogo,music,technologies39,happy29,reporting46,transporting59}
